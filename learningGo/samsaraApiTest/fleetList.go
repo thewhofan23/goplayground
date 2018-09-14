19c1bf30db546e0c63dfd5e69b5f1588134cb650
Alex Govan <alexandertgovan@aol.com>
2018-09-13 20:12:14 -0700

    adding local file with access token, removing actual key for initial commit, then putting into gitignore

--- learningGo/samsaraApiTest/fleetList.go
+++ learningGo/samsaraApiTest/fleetList.go
@@ -6,6 +6,7 @@ import (
 	"fmt"
 	"io/ioutil"
 	"net/http"
+	"os"
 )
 
 // Vehicle - testing comment
@@ -22,9 +23,25 @@ type Fleetlist struct {
 	EngineHours    int
 }
 
+// Configuration - Importing access token from config.json
+type Configuration struct {
+	Accesstoken string
+}
+
 func main() {
 
-	url := "https://api.samsara.com/v1/fleet/list?access_token=RN8Lb4AbjogbP6jQd9EO6AZ2UdPafQ"
+	// Read in the access token from an untracked local file
+	file, err := os.Open("config.json")
+	if err != nil {
+		fmt.Println("File failed: ", err)
+	}
+	defer file.Close()
+	byteValue, _ := ioutil.ReadAll(file)
+	var config Configuration
+	json.Unmarshal(byteValue, &config)
+
+	// Generate the API query
+	url := "https://api.samsara.com/v1/fleet/list?access_token=" + config.Accesstoken
 	postdata := []byte(`{"groupId": 3991}`)
 	req, err := http.NewRequest("GET", url, bytes.NewBuffer(postdata))
 
